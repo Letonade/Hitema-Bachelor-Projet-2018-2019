34caa0b2bf13a94cccdfdbb2bbcef5b37cd64554
Unknown <ones.azouzi96@gmail.com>
2019-05-07 15:40:47 +0200

    maj

--- LeSite/EcranVue.php
+++ LeSite/EcranVue.php
@@ -4,6 +4,48 @@
 ?>
 <html>        
     <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  height: 100%;
+  width: 160px;
+  position: fixed;
+  z-index: 1;
+  top: 56;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+  color: white;
+}
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.main {
+  margin-left: 160px; /* Same as the width of the sidenav */
+  font-size: 28px; /* Increased text to enable scrolling */
+  padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
         <title>Accueil</title>
         <!-- Les includes communs -->
         <?php include $MyHomePath.'assets/inc/head.php'; ?>
@@ -14,7 +56,7 @@
         <?php include $MyHomePath.'assets/inc/include_allModule.php'; ?>
     </head>
 	<body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           	<a class="navbar-brand" href="#">CAPSA CONTAINER</a>
           	<button class="navbar-toggler" type="button" >
             	<span class="navbar-toggler-icon"></span>
@@ -39,10 +81,33 @@
                   </div>
               </div>
           </div>
-          <section>
-            <?php include $MyHomePath.'/assets/php/ContainerList.php'; ?>
-          </section>
-          <section id="TabTuile">
-          </section>
-	</body>
+<div class="sidenav">
+<a href='#'><?php  ?></a>
+<?php
+$connection=mysqli_connect("localhost", "root", "", "capsadomotique");
+// Contrôle sur la connexion
+$requete=mysqli_query($connection,"SELECT id_container FROM container");
+
+mysqli_fetch_array($requete);
+
+
+
+?>
+<div id='#id_container'><?php 
+while($resultat=mysqli_fetch_array($requete)) {
+    echo 'id_container_'. $resultat['id_container']."<br>"."<br>";
+
+    
+    
+ 
+ }
+?> 
+
+<a href="#"><input type="button" value="deco" style="width:100px; height:30px;"></a>
+</div>
+
+</div>
+
+
+  	</body>
 </html>
--- LeSite/assets/php/class/ModuleHabitation.class.php
+++ LeSite/assets/php/class/ModuleHabitation.class.php
@@ -59,20 +59,17 @@ public function AfficherModule($entete){
 				</tr>";
 	}
 	$chaine .= "<td style='border:1px solid black;'>".$date."</td>";
-	$chaine .= "<td style='border:1px solid black;'>".$this->NombreBadge()."</td>";
-	$chaine .= "<td style='border:1px solid black;'>".$this->NombreAparielElectronique()."</td>";
-	$chaine .= "<td style='border:1px solid black;'>".$this->NombreConnexion()."</td>";
-	$chaine .= "<td style='border:1px solid black;'>".$this->Poid()."</td>";
+	$chaine .= "<td style='border:1px solid black;'>".$this->Consommation()."</td>";
 	return($chaine);
 }
 public function get_module($val)
 {
-	$this->IdModuleHabitation($val['id_module_habitation']);
-	$this->NombreBadge($val['nombre_badge']);
+	$this->IdModuleHabitation($val['id_module_gaz']);
+	$this->NombreBadge($val['consomation']);
 	$this->DateChangement($val['date_changement']);
-	$this->Poid($val['poid']);
-	$this->NombreAparielElectronique($val['nombre_apariel_electronique']);
-	$this->NombreConnexion($val['nombre_connexion']);
+	$this->Poid($val['id_container']);
+	$this->NombreAparielElectronique($val['id_container']);
+	$this->NombreConnexion($val['id_container']);
 	
 	
 }
